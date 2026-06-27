31c969f47f7e70c0adefa55042c36f6b0848a359
tamarge <tamargerson5@gmail.com>
2026-06-28 00:29:48 +0300

    reload the main page also if user iside it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,15 @@ interface HeaderProps {
 }
 
 export default function Header({ atTop = false }: HeaderProps) {
+  const handleTitleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const hash = window.location.hash;
+    // If the hash is empty or '#', we are on the landing page, so reload the page
+    if (!hash || hash === '#') {
+      e.preventDefault();
+      window.location.reload();
+    }
+  };
+
   return (
     <header className={`site-header ${atTop ? 'at-top' : ''}`}>
       <div className="header-content">
@@ -13,7 +22,7 @@ export default function Header({ atTop = false }: HeaderProps) {
         </nav>
 
         <div className="header-title-container">
-          <a href="#" className="header-title-link">
+          <a href="#" className="header-title-link" onClick={handleTitleClick}>
             <h1 className="header-title">קפיטליזם זו לא מילה גסה</h1>
           </a>
         </div>
